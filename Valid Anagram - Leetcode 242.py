7ad166b67045f7dab26b0e1c8e727380e70bb3a5
Daivik Dhar <165635276+dhardaiv@users.noreply.github.com>
2024-09-19 16:55:51 -0400

    Update Valid Anagram - Leetcode 242.py

--- Valid Anagram - Leetcode 242.py
+++ Valid Anagram - Leetcode 242.py
@@ -1,1 +1,15 @@
 
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) == len(t): 
+            for i in set(s):
+                if s.count(i) != t.count(i): 
+                    return False 
+            return True
+        else: 
+            return False
